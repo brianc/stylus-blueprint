b06b3048fa19cbdb63ed246adf9d25857a0283dc
Brian Carlson <brian.m.carlson@gmail.com>
2011-03-26 16:53:04 -0500

    refactor for readability

--- test/server.js
+++ test/server.js
@@ -1,49 +1,49 @@
-var connect = require('connect');
-
-var server = connect(connect.favicon());
-
 var fs = require('fs');
 
+var stylus = require('stylus');
+var blueprint = require(__dirname + '/../lib');
+var connect = require('connect');
 var jade = require('jade');
 
-var stylus = require('stylus');
-var blueprint = require(__dirname + '/../../stylus-blueprint/lib');
 
-var stylusServer = function(req, res, next) {
-  var path = req.params['style'];
-  fs.readFile(path+".styl", 'utf8', function(err, contents) {
-    if(err) return next(err);
-    console.log("stylus path %s", blueprint);
-    stylus(contents)
-      .set('filename', path)
-      .set('paths', [blueprint])
-      .render(function(err, css) {
-        if(err) return next(err);
-        res.writeHead(200, {'content-type':'text/css'})
-        res.end(css);
-      })
-  })
-}
+var router = connect.router(function(app) {
 
-
-server.use(connect.router(function(app) {
-  app.get("/:style.css", stylusServer)
-  app.get("/:page.html", function(req, res, next) {
-    jade.renderFile(__dirname + "/" + req.params['page'] + ".jade", function(err, htm) {
+  var render = function(file, res, next) {
+    jade.renderFile(__dirname + "/" + file + ".jade", function(err, htm) {
       if(err) return next(err);
       res.writeHead(200, {'content-type':'text/html'});
       res.end(htm);
     })
+  };
+
+  var stylusServer = function(req, res, next) {
+    var path = req.params['style'];
+    fs.readFile(__dirname + "/" + path + ".styl", 'utf8', function(err, contents) {
+      if(err) return next(err);
+      stylus(contents)
+        .set('filename', path)
+        .set('paths', [blueprint])
+        .render(function(err, css) {
+          if(err) return next(err);
+          res.writeHead(200, {'content-type':'text/css'})
+          res.end(css);
+        })
+    })
+  };
+
+  app.get("/:style.css", stylusServer)
+  app.get("/:page.html", function(req, res, next) {
+    var file = req.params['page'];
+    render(file, res, next);
   })
-}))
+  app.get('/', function(req, res, next) {
+    render("index", res, next);
+  })
+})
 
-server.use(connect.static(__dirname))
-
-server.use(function(req, res) {
-  res.writeHead(200, {'content-type':'text/html'});
-  var html = [];
-  html.push('<html><head><link rel="stylesheet" href="first.css" type="text/css" /></head><body>')
-  html.push('<div>Hi there</div>')
-  html.push('</body></html>')
-  res.end(html.join(''));
-}).listen(3001);
+var server = connect();
+server.use(router)
+server.use(connect.static(__dirname));
+server.use(connect.favicon());
+server.use(connect.errorHandler({showStack: true, dumpExceptions: true})).listen(3001);
+console.log("Server listening on 3001");
